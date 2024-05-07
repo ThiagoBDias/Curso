46200372219ee205cf51ecd11b1d35763e03f34f
Thiago Batista <Thiagobatistadiass@gmail.com>
2024-05-07 15:38:34 -0300

    conteudo do curso

--- Js1Aula/index.js
+++ Js1Aula/index.js
@@ -1,1 +1,12 @@
 console.log('"NOME Thiago baitsta dias"Estou apredendo JavaScript ás', 10 ,'da manhã.');
+
+// string 
+let nome = 'joao';
+
+console.log(nome,' fico feliz ');
+
+// podemos declarar variaveus depois  exmplo 
+let nome2 ;
+ nome2 = 'teste';
+
+console.log('testes',nome2);
---
+++ Js4Aula/index.js
@@ -0,0 +1,16 @@
+// calculando o icms
+
+const nome = 'otavio';
+const sobrenome ='souza';
+const idade =17;
+const pesso =84;
+const altura = 1.80;
+let pessoatual;
+let anonacimento ;
+
+pessoatual = peso / (altura*altura);
+anonacimento = 2019-idade;
+
+//template strint
+
+console.log('${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg');

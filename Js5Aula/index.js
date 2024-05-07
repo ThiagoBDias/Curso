df8b418977059477b2ae39d1bcd390c3b5b5fef7
Thiago Batista <Thiagobatistadiass@gmail.com>
2024-05-07 17:15:55 -0300

    comeco da aula

---
+++ Js5Aula/index.js
@@ -0,0 +1,15 @@
+//  Diferenteça entre as  var e let 
+// Var poder ser declarada em sima de outro var 
+// quando ussamos let  não podemos redeclarar variavel 
+//  temos string 
+const nome = 'tico';
+// podemos ter number 
+const num1 = 10;
+//podemos ter valor nulo não posta valor da menoria undefined 
+let nomeAluno;
+// podemos colocar o valor usuario 
+const sobrenomeAluno = null;
+// quando usar o valor nulo 
+const aprovado = false; // boolear  true ou false
+// para poder ver o tipo da variavel vc pode typeof 
+console.log(typeof nome);

1ec0c0408feb0c94a7df4ad853da583052e30b48
Thiago Batista <Thiagobatistadiass@gmail.com>
2024-05-16 17:42:35 -0300

    subi

---
+++ js6Aula/index.js
@@ -0,0 +1,14 @@
+/**
+ *  Aritmeticos 
+ * + adição / Concatenação
+ * - menos  / 
+ * / dividir 
+ * 
+ *  ** potuação 
+ * % Resto da divisão 
+ *   */
+
+const num1 = 5;
+const num2 = 50 ; 
+console.log(num1 + num2);
+

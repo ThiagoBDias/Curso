30d908e8a99c730878b6b141ec7761000d006c18
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-01-31 08:47:47 -0300

    Update index.js

--- Js10Aula/index.js
+++ Js10Aula/index.js
@@ -1,17 +1,19 @@
-let num1 = 9.548;
+//let num1 = 9.548;
 // arrendodar para baixo FLOOR 
-let num2 = math.floor(num1);
+//let num2 = math.floor(num1);
 // arrendodar para cima CEIL
 
-let num23 = math.ceil(num1);
+//let num23 = math.ceil(num1);
 
-console.log(num2);
+//console.log(num2);
 // round arrendoda para o mais proximo 
-let num3 = math.round();
-console.log(num2);
+//let num3 = math.round();
+//console.log(num2);
 
 //math.max faz vc encontrar o numero maior do seu pedido 
 
-console.log(math.max(1,2,3,5,6,4,8,542));
-//
-console.log(math.min(1,2,3,5,6,4,8,542));
+//console.log(math.max(1,2,3,5,6,4,8,542));
+
+//math.min faz vc encontrar o numero menor do seu pedido 
+//console.log(math.min(1,2,3,5,6,4,8,542));
+

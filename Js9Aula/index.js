9c1528b2dd1033da7db1004c5b7148f648c1273c
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-01-31 08:50:55 -0300

    Update index.js

--- Js9Aula/index.js
+++ Js9Aula/index.js
@@ -1,25 +1,18 @@
 let num1 = 10;
 let num2  = 2.5;
-
 //
 //console.log(num1.toString() + num2);
 //typeof verificar 
-
 //console.log(typeof num1);
-
 console.log(num1.toString(2));
 //colocar em binario o tostring
-
 console.log(num1.toFixed(2));
 //ter um certificado depois do ponto .
-
 console.log(number.isInteger(num1));
 //  verificar se e verdadeiro 
-
 let temp = num1 *'ola'; 
 console.log(number.isNaN(temp));
 // colocar verdader se a conta e validado 
-
 // parseFloat - para colocar o numero decimal 
 
 

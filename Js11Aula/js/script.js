9b9e5a64d1a11ecce651228d4a881eab5f535917
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-02-04 17:36:36 -0300

    Update script.js

--- Js11Aula/js/script.js
+++ Js11Aula/js/script.js
@@ -1,7 +1,7 @@
-let numero = prompt( 'Escreva um numero:' );
-numero = number(numero);
+const numero = Number(prompt( 'Escreva um numero:'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
 texto.innerHTML =`<p>seu numero - 2 é ${numero-2}.<\p> `;
+ 

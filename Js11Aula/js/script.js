e949f44178189b8bdcd52e44581cefb038af912a
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-02-04 16:52:34 -0300

    Update script.js

--- Js11Aula/js/script.js
+++ Js11Aula/js/script.js
@@ -1,2 +1,5 @@
 const numero = prompt( 'Escreva um numero:' );
-document.getElementById
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;

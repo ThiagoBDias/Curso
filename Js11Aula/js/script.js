5f93ec7c80a0cd605e486cfc937959d2d6ed160d
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-02-04 17:57:43 -0300

    finaliado

--- Js11Aula/js/script.js
+++ Js11Aula/js/script.js
@@ -3,6 +3,7 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
+texto.innerHTML = '';
 texto.innerHTML += `<p>Raiz quadrada ${numero ** 0.5}.<\p> `;
 texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}.</p> `;
 texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}.</p> `;

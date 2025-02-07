de445e9045e77c80a3f9f9e1edc653dd3c040fb9
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-02-07 10:21:17 -0300

    Update index.js

--- Js12Aula/index.js
+++ Js12Aula/index.js
@@ -9,11 +9,14 @@ cada coxite tem sua possição que seria do incio de 0
  alunos[alunos.length] = 'filipe';
 
  push podeser oara adicional o final 
-
   push
+
+  adicionar no começo 
+  unshift 
+alunos.unshift('ze');
   
 */
 
 const alunos = ['maria', 'ze','felipe'];
-co
+
 console.log(alunos); 

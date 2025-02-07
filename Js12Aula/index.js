2cbfcba699e2a873e2815dea70914817927af38b
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-02-07 09:32:42 -0300

    Update index.js

--- Js12Aula/index.js
+++ Js12Aula/index.js
@@ -4,10 +4,14 @@ A arraya e uma lista de coisa podemos ultilizar os cocete []
 cada coxite tem sua possição que seria do incio de 0 
  para usar a possição vc usa o 0 
  alunos[3] = 'luiza';
- 
+
  colocar ua possição no array 
  alunos[alunos.length] = 'filipe';
 
+ push podeser oara adicional o final 
+
+  push
+  
 */
 
 const alunos = ['maria', 'ze','felipe'];

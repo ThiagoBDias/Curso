658fa883622bc151fa7fe0edb82a87fd83f56605
Thiago Batista <Thiagobatistadiass@gmail.com>
2025-02-07 17:57:05 -0300

    Update index.js

--- Js13Aula/index.js
+++ Js13Aula/index.js
@@ -1,7 +1,14 @@
-
+/*
 function saudacao(nome){
 
  return `Bom dia ${nome}!`;
 }
+
 const variavel = saudacao('ze');
 console.log(variavel);
+*/
+
+function soma(x, y){
+const resultado = x+y;
+return resultado;
+}

bae5205f515d6dfd1eb33f39f2822759125f3bd3
Thiago Dias <thiagobatistadiass@gmail.com>
2025-08-25 14:56:56 -0300

    atualização

--- Js19Aula/Index.js
+++ Js19Aula/Index.js
@@ -1,5 +1,15 @@
-const hora = 10;
+/*
+    entre  0- 11 - Bom dia 
+    Entre 12 - 17 - Boa tarde
+    Entre as 18 - 23- Boa noite
+*/ 
 
-if (hora <12 ){
+const hora = 13;
+
+if (hora >=0 && hora <= 11 ){
     console.log('bom dia !');
-}
+} else if(hora >= 12 && hora <= 17){
+     console.log('Boa tarde !');
+}  else if(hora >=18 && hora <= 23){
+     console.log('Boa noite !');
+ }
